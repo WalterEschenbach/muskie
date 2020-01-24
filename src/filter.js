048abf1e3766840081f2d141f44cf27abbf6e833
Walter <eschenbach.walter@gmail.com>
2020-01-24 02:18:09 -0500

    desktop layout troubleshooting

--- src/filter.js
+++ src/filter.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 
 const Filter = props => (
-    <header className="filter">
         <nav className="filterNav">
-            <div></div>
             <div className="filterContainer">
                 <ul>
                     <li><a href='#'>Reels</a></li>
@@ -14,7 +12,7 @@ const Filter = props => (
                 </ul>
             </div>
         </nav>
-    </header>
+   
 );
 
 export class DesktopFilter extends React.Component {
--- src/shop.js
+++ src/shop.js
@@ -2,13 +2,6 @@ import React from 'react';
 
 let log = console.log;
 
-// const xhr = new XMLHttpRequest();
-// xhr.open('GET', 'http://localhost:8000/products');
-// xhr.onload = () => {
-//     const products = JSON.parse(xhr.responseText);
-//     log(products[0]);
-// };
-// xhr.send();
 
 fetch("http://localhost:8000/products/")
     .then(response => response.text())
@@ -19,18 +12,10 @@ fetch("http://localhost:8000/products/")
 
 export class Shop extends React.Component {
 
-
-
-
-
     render() {
         return(
             <div className="shopContainer">
-                
-                <div>SHOP
-
-                </div>
-                
+                <div><h6>SHOP</h6></div>
             </div>
         )
     }
